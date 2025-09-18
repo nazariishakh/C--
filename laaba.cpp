cf450b1dfd5b8996904b38b7babdeb077fc35344
Nazarii Shakh <nazarii.shakh.shi.2025@lpnu.ua>
2025-09-18 14:21:49 +0300

    commit3

--- laaba.cpp
+++ laaba.cpp
@@ -3,17 +3,20 @@
 using namespace std;
 
 int x, y, z;
-
+// 32424243fdssfsdff
 int main()
 {
 
-        cout << "Введіть три числа\n";
+    cout << "Введіть три числа\n";
     cin >> x >> y >> z;
     cout << "Ваші числа: " << x << " " << y << " " << z << endl
          << (!y) << endl
          << (!z) << endl
          << (y && (!z)) << endl
-         << (x || y) << endl;
+         << (x || y) << endl
+         << (x || (!y)) << endl
+         << (((!x) && y) || (((!y) || z) || (x || y))) << endl;
     return 0;
 }
 //(𝑥 ∨ ¬𝑦) ⟶ ((𝑦 ∧ ¬𝑧) → (𝑥 ∨ 𝑦));
+// if x or y or z == 0 or x or y or z == 1 {
